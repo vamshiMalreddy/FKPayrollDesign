fea5ffa6eff80c7c5534c2183a0e2552386b1119
Vamshi Malreddy <malreddyvamshi05@gmail.com>
2020-05-10 16:40:37 +0530

    Union Membership also is taken as input

--- SourceCode/Payroll.java
+++ SourceCode/Payroll.java
@@ -7,13 +7,13 @@ abstract class Employee{
 	protected String method_of_payment;
 	protected boolean union_membership;
 
-	Employee(int ID, String Name,double commission_rate,String method_of_payment)
+	Employee(int ID, String Name,double commission_rate,String method_of_payment,boolean union_membership)
 	{
 		this.ID = ID;
 		this.Name = Name;
 		this.commission_rate = commission_rate;
 		this.method_of_payment = method_of_payment;
-		this.union_membership = false;
+		this.union_membership = union_membership;
 	}
 
 	public void toggle_union_membership()
@@ -30,9 +30,9 @@ class Work_by_hour_employee extends Employee{
 	private double hourly_pay_rate;
 	private double amount_to_be_paid;
 
-    Work_by_hour_employee(double hourly_pay_rate,int ID, String Name,double commission_rate,String method_of_payment)
+    Work_by_hour_employee(double hourly_pay_rate,int ID, String Name,double commission_rate,String method_of_payment,boolean union_membership)
 	{
-		super(ID,Name,commission_rate,method_of_payment);
+		super(ID,Name,commission_rate,method_of_payment,union_membership);
 		this.amount_to_be_paid = 0;
 		this.hourly_pay_rate = hourly_pay_rate;
 	}
@@ -62,13 +62,13 @@ class Work_by_hour_employee extends Employee{
 
 }
 
-class Flat_salary_employees extends Employee{
+class Flat_salary_employee extends Employee{
 	private double salary;
 	private double amount_to_be_paid;
 
-	Flat_salary_employees(double salary,int ID, String Name,double commission_rate,String method_of_payment)
+	Flat_salary_employee(double salary,int ID, String Name,double commission_rate,String method_of_payment,boolean union_membership)
 	{
-		super(ID,Name,commission_rate,method_of_payment);
+		super(ID,Name,commission_rate,method_of_payment,union_membership);
 		this.amount_to_be_paid = salary;
 		this.salary = salary;
 	}
@@ -94,8 +94,8 @@ class Flat_salary_employees extends Employee{
 
 public class Payroll{
 	public static void main(String[] args) {
-		Employee emp = new Work_by_hour_employee(4.5,18,"Vamshi",1.8,"pickup by the paymaster");
-		Employee emp1 = new Flat_salary_employees(555,41,"Paxton",6.5,"pickup by the paymaster");
+		Employee emp = new Work_by_hour_employee(4.5,18,"Vamshi",1.8,"pickup by the paymaster",true);
+		Employee emp1 = new Flat_salary_employee(555,41,"Paxton",6.5,"pickup by the paymaster",true);
 		emp1.submit_commission_receipt(45,new Date(2020,5,10));
 	}
 }
